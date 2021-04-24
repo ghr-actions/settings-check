10d2674179b9ee95313d091b3cde23d450e97342
Gavin Fenton <gavin_fenton@msn.com>
2021-04-24 11:21:50 +0100

    Started adding errors

--- src/config.ts
+++ src/config.ts
@@ -8,9 +8,42 @@ export interface Config {
   token: string
 }
 
-const getRepositoryList = (repositoriesString: string): string[] =>
-  repositoriesString.split(',').map((r) => r.trim())
+const validRepo = new RegExp('^[w.-_]+$')
+const validOwner = new RegExp('^[w-]+$')
 
+const getRepositoryList = (repositoriesString: string): string[] => {
+  return repositoriesString
+    .split(',')
+    .map((r) => r.trim())
+    .map((r) => {
+      const [part1, part2, ...rest] = r.split('/')
+      let owner, repo
+
+      if (rest.length != 0) {
+        throw new Error(`Repository ${r} is not valid`)
+      }
+
+      if (part1 == undefined) {
+        ;[owner, repo] = (process.env.GITHUB_REPOSITORY || '/').split('/')
+      } else if (part2 == undefined) {
+        repo = part1
+        owner = (process.env.GITHUB_REPOSITORY || '/').split('/')[0]
+      } else {
+        owner = part1
+        repo = part2
+      }
+
+      if (!validRepo.test(repo)) {
+        throw new Error(`Repository name ${repo} is not valid`)
+      }
+
+      if (!validOwner.test(owner)) {
+        throw new Error(`Owner name ${owner} is not valid`)
+      }
+
+      return `${owner}/${repo}`
+    })
+}
 const getRules = async (
   rulesPath: string
 ): Promise<Record<string, boolean>> => {
